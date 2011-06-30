1a19c47820cf43c1eeda6a301b79c6121d17e15d
twixt01 <twixt01@gmail.com>
2011-06-30 16:56:05 +0000

    initial setup script

---
+++ initial_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Setting permissions, installing pre-reqs"
+sudo apt-get install -y screen
+sudo chmod 777 /var/run/screen
+sudo apt-get install -y sendemail
+sudo apt-get install -y libnet-ssleay-perl
+sudo apt-get install -y libio-socket-ssl-perl
+echo "All done. Be sure to set your setting in start_miner_x";
+

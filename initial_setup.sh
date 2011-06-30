a802a5c39c3d669c9f0245121ea5bc0a0b263f24
twixt01 <twixt01@gmail.com>
2011-06-30 09:58:33 -0700

    Edited initial_setup.sh via GitHub

--- initial_setup.sh
+++ initial_setup.sh
@@ -6,5 +6,5 @@ sudo chmod 777 /var/run/screen
 sudo apt-get install -y sendemail
 sudo apt-get install -y libnet-ssleay-perl
 sudo apt-get install -y libio-socket-ssl-perl
-echo "All done. Be sure to set your setting in start_miner_x";
+echo "All done. Be sure to set your settings in start_miner_x";
 
